43459196176980da775f54afba0075258a7d04c9
Raitis Stengrevics <dagrevis@gmail.com>
2015-01-16 20:53:26 +0200

    Improve code, add comments

--- src/hndrx/core.cljs
+++ src/hndrx/core.cljs
@@ -5,7 +5,13 @@
             [schema.core :as schema :include-macros true])
   (:import [goog.string format]))
 
-(enable-console-print!)
+; Utilities.
+
+(defn debug! [what]
+  (.debug js/console what))
+
+(defn error! [what]
+  (.error js/console what))
 
 (defn uuid4!
   "https://catamorphic.wordpress.com/2012/03/02/generating-a-random-uuid-in-clojurescript/"
@@ -23,7 +29,6 @@
 (defn connect-to-peerserver
   ([peer-id]
    (connect-to-peerserver peer-id {:host "localhost" :port 9000}))
-
   ([peer-id options]
    (js/Peer. peer-id (clj->js options))))
 
@@ -48,32 +53,74 @@
 (defn on-connection-data [connection callback]
   (.on connection "data" #(callback (js->clj % :keywordize-keys true))))
 
+; Schemas.
+
+(def Message {:body schema/Str
+              :from schema/Str
+              :to [schema/Str]})
+
+; Safe functions.
 
 (defn prepare-data [data-type data-source]
   {:data-type data-type
    :data-source data-source})
+
+; App state.
+
+(def status
+  (atom :undecided))
+
+(def peer-id (atom (subs (uuid4!) 0 36)))
+(def peer (atom (connect-to-peerserver @peer-id)))
+(def connections (atom []))
+(def messages (atom []))
+
+; Unsafe functions.
+
+(defn undecided? []
+  (= @status :undecided))
+(defn leader? []
+  (= @status :leader))
+(defn follower? []
+  (= @status :follower))
 
 (defn send-message! [connections message]
   (let [data (prepare-data "message" message)]
     (doseq [connection connections]
       (send-data! connection data))))
 
-(def Message {:body schema/Str
-              :from schema/Str
-              :to [schema/Str]})
-
-(def connections (atom []))
-(def messages (atom []))
+; Channels and events.
 
 (def connections-chan (chan))
 (def data-chan (chan))
 (def messages-chan (chan))
 
-; Called when a connection is made. Called once when follower connects to the leader on both sides.
+(defn on-connection [connection]
+  (put! connections-chan connection))
+
+(defn on-connection-from-undecided []
+  (debug! "new connection from undecided"))
+
+;; TODO: When there is new connection, send all current peer-ids to all connections leader knows.
+(defn on-connection-from-leader []
+  (debug! "new connection from leader"))
+
+(defn on-connection-from-follower []
+  (debug! "someone connected from follower, this should not happen"))
+
+(on-peer-connection @peer on-connection)
+(on-peer-error @peer #(error! (.-type %)))
+
+;; Called on any kind of new connection.
 (go
   (loop []
     (let [connection (<! connections-chan)]
       (on-connection-data connection #(put! data-chan %))
+
+      (cond
+        undecided? (on-connection-from-undecided)
+        leader? (on-connection-from-leader)
+        follower? (on-connection-from-follower))
 
       (swap! connections conj connection))
 
@@ -88,7 +135,7 @@
 
     (recur)))
 
-; There is no difference between local messages and ones that are sent to you over the network.
+;; There is no difference between local messages and ones that are sent to you over the network.
 (go
   (loop []
     (let [message (<! messages-chan)]
@@ -98,33 +145,23 @@
 
     (recur)))
 
-(def peer-id (subs (uuid4!) 0 36))
-(def peer (connect-to-peerserver peer-id))
-
-(defn on-connection-to-leader [connection]
-  (put! connections-chan connection)
-
-  ; TODO: When there is new connection, send all current peer-ids to all connections leader knows.
-  )
-
-(on-peer-connection peer on-connection-to-leader)
-
-(on-peer-error peer #(println (.-type %)))
+; Components.
 
 (defn connections-component []
   [:div
    [:h2 "Connections"]
    [:ul
     (for [peer-id (connections->peer-ids @connections)]
-      ^{:key peer-id} [:li peer-id])]])
+      ^{:key peer-id}
+      [:li peer-id])]])
 
 (defn connecting-component []
   (let [peer-id-to-connect-to (atom "")]
     (fn []
       [:form {:on-submit (fn [e]
                            (.preventDefault e)
-                           (let [connection (new-connection peer @peer-id-to-connect-to)]
-                             (.on connection "error" #(println "Could not connect to other peer"))
+                           (let [connection (new-connection @peer @peer-id-to-connect-to)]
+                             (.on connection "error" #(error! "Could not connect to other peer"))
                              (.on connection "open" #(put! connections-chan connection)))
                            (reset! peer-id-to-connect-to ""))}
        [:input {:type "text"
@@ -137,6 +174,7 @@
    [:h2 "Messages"]
    [:ul
     (for [{:keys [body from]} @messages]
+      ^{:key [body from]}
       [:li body " (by " [:code from] ")"])]])
 
 (defn messaging-component []
@@ -146,7 +184,7 @@
                            (.preventDefault e)
 
                            (let [message {:body @value
-                                          :from peer-id
+                                          :from @peer-id
                                           :to (connections->peer-ids @connections)}]
                              (schema/validate Message message)
 
@@ -162,10 +200,12 @@
 (defn root-component []
   [:div
    [:h1 "Hndrx"]
-   [:p "Your peer-id is " [:code peer-id]]
+   [:p "Your peer-id is " [:code @peer-id]]
    [connections-component]
    [connecting-component]
    [messages-component]
    [messaging-component]])
+
+; Takeoff for graphics!
 
 (reagent/render-component [root-component] (.-body js/document))
